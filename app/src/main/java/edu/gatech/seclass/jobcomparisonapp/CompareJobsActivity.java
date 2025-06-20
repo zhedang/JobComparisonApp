1ce6035a6eb411b2efa169f3e5805b844552600e
Zhe Dang <raymanic@gmail.com>
2025-06-20 13:46:41 -0300

    Implement data binding for CompareJobsActivity: display saved jobs

--- app/src/main/java/edu/gatech/seclass/jobcomparisonapp/CompareJobsActivity.java
+++ app/src/main/java/edu/gatech/seclass/jobcomparisonapp/CompareJobsActivity.java
@@ -7,6 +7,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 import java.util.ArrayList;
+import java.util.List;
 
 public class CompareJobsActivity extends AppCompatActivity {
 
@@ -21,10 +22,19 @@ public class CompareJobsActivity extends AppCompatActivity {
         jobListView = findViewById(R.id.jobListView);
         jobList = new ArrayList<>();
 
-        // Dummy data for now, will connect with model later
-        jobList.add("Current Job: Software Engineer at Google");
-        jobList.add("Offer 1: Senior Developer at Amazon");
-        jobList.add("Offer 2: Software Engineer II at Microsoft");
+        // Load data from the model
+        if (MainActivity.jobApp.getCurrentJob() != null) {
+            Job currentJob = MainActivity.jobApp.getCurrentJob();
+            jobList.add("Current Job: " + currentJob.getTitle() + " at " + currentJob.getCompany()
+                    + " (" + currentJob.getLocation().getCity() + ", " + currentJob.getLocation().getState() + ")");
+        }
+
+        List<Job> offers = MainActivity.jobApp.getJobOffers();
+        for (int i = 0; i < offers.size(); i++) {
+            Job job = offers.get(i);
+            jobList.add("Offer " + (i + 1) + ": " + job.getTitle() + " at " + job.getCompany()
+                    + " (" + job.getLocation().getCity() + ", " + job.getLocation().getState() + ")");
+        }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, jobList);
         jobListView.setAdapter(adapter);

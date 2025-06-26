93a41fcbaa0d5a2b91f2d23771faa2845e7b011f
Zhe Dang <raymanic@gmail.com>
2025-06-26 17:24:06 -0300

    create xml for main and entercurrentjob with navigation. next step,sql save function.

---
+++ app/src/main/java/edu/gatech/seclass/jobcompare6300/EnterCurrentJob.java
@@ -0,0 +1,20 @@
+package edu.gatech.seclass.jobcompare6300;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatActivity;
+
+// Setting up EnterCurrentJob navigation
+public class EnterCurrentJob extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_current_job);
+
+    }
+
+}
